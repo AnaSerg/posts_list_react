c9d1f14935094796824199e6b76691f912e52fab
Anastasia Sergeeva <lirebook@yandex.ru>
2023-07-17 10:46:07 +0300

    feat: create Navbar and make it adaptive

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import styles from './navbar.module.scss';
+import { MenuItem } from "../MenuItem/MenuItem";
+import Logo from "../../assets/logo.svg";
+
+export const Navbar = ({isMobileMenuOpen, closeMobileMenu}) => {
+    const [isScrolled, setIsScrolled] = useState(false);
+    const [isMobileView, setIsMobileView] = useState(false);
+
+    const menuItems = ['Demos', 'Post', 'Features', 'Categories', 'Shop'];
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 200) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        const handleResize = () => {
+            setIsMobileView(window.innerWidth <= 680);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        window.addEventListener('resize', handleResize);
+
+        handleResize();
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return (
+        <nav className={`${styles.navigation} ${isScrolled ? styles.scrolled : ''}`}>
+            {isMobileView ? (
+                <div className={isMobileMenuOpen ? `${styles.mobileMenu} ${styles.mobileMenuOpened}` : styles.mobileMenu}>
+                    <div className={isMobileMenuOpen ? `${styles.mobileMenuContainer} ${styles.mobileMenuContainerOpened}` : styles.mobileMenuContainer}>
+                        <div className={styles.mobileMenuContainerHeader}>
+                            <img src={Logo} alt="logo" className={styles.logo} />
+                            <button onClick={closeMobileMenu} className={styles.closeBtn}/>
+                        </div>
+                        <ul className={styles.list}>
+                            {menuItems.map((item) => (
+                                <MenuItem key={item} item={item} />
+                            ))}
+                        </ul>
+                        <a href="#" className={styles.button}>Buy Now</a>
+                    </div>
+                </div>
+            ) : (
+                <>
+                    {!isMobileView && (
+                        <ul className={styles.list}>
+                            {menuItems.map((item) => (
+                                <MenuItem key={item} item={item} />
+                            ))}
+                        </ul>
+                    )}
+                    <a href="#" className={styles.button} >Buy Now</a>
+                </>
+            )}
+        </nav>
+    );
+};
+
+
